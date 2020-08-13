0358122b4d78d68062a300075177140c7cce6eca
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-13 22:11:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,32 +1,23 @@
 def addition(num1, num2)
-sum = num1 + num2
-puts sum
-sum
+num1 + num2
 end
 
 def subtraction(num1, num2)
-dif = num1 - num2
-puts dif
-dif
+num1 - num2
 end
 
 def division(num1, num2)
-div = num1 / num2
-puts div
-div
+num1 / num2
 end
 
 def multiplication(num1, num2)
-mul = num1 * num2
-puts mul
-mul
+num1 * num2
 end
 
 def modulo(num1, num2)
-
 num1 % num2
 end
 
 def square_root(num)
-puts num*num
+num*num
 end
