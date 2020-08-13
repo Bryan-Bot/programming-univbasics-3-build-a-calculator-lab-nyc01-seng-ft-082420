ff48d35ca3d352348c0472860c8db421951a6443
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-13 22:09:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,30 @@
 def addition(num1, num2)
-puts num1 + num2
+sum = num1 + num2
+puts sum
+sum
 end
 
 def subtraction(num1, num2)
-puts num1 - num2
+dif = num1 - num2
+puts dif
+dif
 end
 
 def division(num1, num2)
-puts num1 / num2
+div = num1 / num2
+puts div
+div
 end
 
 def multiplication(num1, num2)
-puts num1 * num2
+mul = num1 * num2
+puts mul
+mul
 end
 
 def modulo(num1, num2)
-puts num1 % num2
+
+num1 % num2
 end
 
 def square_root(num)
